ade14e3e8c1dff04a1b2308ae4da254a5457491f
ElChauve <florent.flecheau@etu.univ-tours.fr>
2016-10-14 15:48:05 +0200

    Dev Threads

--- Projet_DI4_SynthetiseurMusical/src/Model/GestionThreads.java
+++ Projet_DI4_SynthetiseurMusical/src/Model/GestionThreads.java
@@ -1,5 +1,67 @@
 package Model;
 
-public class GestionThreads {
+import java.util.ArrayList;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+
+public class GestionThreads extends Thread{
+	protected static final int SAMPLE_RATE = 128 * 1024;
+	private int TEMPO = 120; //Nb de noires par min
+	private Partition part;
+	
+	public GestionThreads(Partition part) {
+		this.part=part;
+	}
+
+	public void run() {
+		final AudioFormat af = new AudioFormat(GenerateurSon.SAMPLE_RATE, 8, 1, true, true);
+		SourceDataLine line = null;
+		try {
+			line = AudioSystem.getSourceDataLine(af);
+		} catch (LineUnavailableException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			line.open(af, GenerateurSon.SAMPLE_RATE);
+		} catch (LineUnavailableException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		line.start();
+		
+		ArrayList<Note> noteList = part.getNotes();
+		
+		for(Note n : noteList){
+			if(n.getHauteur() == Octave.NONE)
+				try {
+					Thread.sleep(n.getDuree());
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			else{
+				byte[] output = createSinWaveBuffer(n.getFrequence(), n.getDuree());
+				line.write(output, 0, output.length);
+			}
+		}
+		
+		line.drain();
+		line.close();
+	}
+
+	public static byte[] createSinWaveBuffer(double freq, int ms) {
+		int samples = (int) ((ms * SAMPLE_RATE) / 1000);
+		byte[] output = new byte[samples];
+		//
+		double period = (double) SAMPLE_RATE / freq;
+		for (int i = 0; i < output.length; i++) {
+			double angle = 2.0 * Math.PI * i / period;
+			output[i] = (byte) (Math.sin(angle) * 127f);
+		}
+		return output;
+	}
 
 }
--- Projet_DI4_SynthetiseurMusical/src/Model/Main.java
+++ Projet_DI4_SynthetiseurMusical/src/Model/Main.java
@@ -2,22 +2,26 @@ package Model;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 
 public class Main {
 
 	public static void main(String[] args) throws LineUnavailableException {
-		Partition part = null;
+		Partition part1 = null, part2 = null;
 		try {
-			part = new Partition("greensleeves.txt");
+			part1 = new Partition("greensleeves.txt");
+			part2 = new Partition("greensleevesV2.txt");
 			//part = new Partition("test.txt");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		
-		GenerateurSon.jouerMelodie(part);
+		GestionThreads t1 = new GestionThreads(part1);
+		GestionThreads t2 = new GestionThreads(part2);
+		t1.start();
+		t2.start();
+		/*GenerateurSon.jouerMelodie(part);*/
 	}
 
 }
