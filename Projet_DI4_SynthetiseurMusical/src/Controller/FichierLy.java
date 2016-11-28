d2562ed07ff8d13beeaa24e6d4fd963870af88db
Julien A <Edroxis@users.noreply.github.com>
2016-11-28 09:27:25 +0100

    more flexible txt files

--- Projet_DI4_SynthetiseurMusical/src/Controller/FichierLy.java
+++ Projet_DI4_SynthetiseurMusical/src/Controller/FichierLy.java
@@ -14,13 +14,50 @@ public class FichierLy {
 		this.nomFichier = nomFichier;
 		contenuFichier = "";
 		contenuFichier = extraireContenu();
+		formaterContenu(contenuFichier);
 	}
 
 	public String getContenu() {
 		return contenuFichier;
 	}
 
-	/*Source: http://www.commentcamarche.net/forum/affich-590149-lire-un-fichier-texte-en-java*/
+	public String formaterContenu(String contenuFichier) throws Exception {
+		String NouveauContenu = "";
+		int compt = 0, NbVoix = 0, NumVoix = 0;
+
+		// On split la chaîne de caractères par rapport aux retours à la ligne
+		String Tableau[] = contenuFichier.split("\n");
+
+		// Calcul du nombre de voix grâce aux lignes vides
+		while (!Tableau[compt].equals("")) {
+			NbVoix++;
+			compt++;
+		}
+
+		// Initialisation du tableau temporaire
+		String TabTemp[] = new String[NbVoix];
+		for (compt = 0; compt < TabTemp.length; compt++) {
+			TabTemp[compt] = "";
+		}
+
+		// On réécrit ligne par ligne dans le tableau temporaire, avec une ligne par voix  
+		for (compt = 0; compt < Tableau.length; compt++) {
+			// Si on dépasse le nombre de voix, la prochaine ligne reviendra à la 1ère voix
+			if ((NumVoix + 1) > TabTemp.length) {
+				NumVoix = NumVoix - NbVoix;
+				compt++;
+			}
+			TabTemp[NumVoix] = TabTemp[NumVoix] + Tableau[compt] + " ";
+			NumVoix++;
+		}
+
+		// On réécrit le tableau temporaire dans un String 
+		for (compt = 0; compt < TabTemp.length; compt++) {
+			NouveauContenu = NouveauContenu + TabTemp[compt] + '\n';
+		}
+		return NouveauContenu;
+	}
+
 	public String extraireContenu() throws Exception {
 		// lecture du fichier texte
 		try {
@@ -36,7 +73,7 @@ public class FichierLy {
 		} catch (Exception e) {
 			System.out.println(e.toString());
 		}
-		contenuFichier = contenuFichier.substring(0, contenuFichier.length()-1);
+		contenuFichier = contenuFichier.substring(0, contenuFichier.length() - 1);
 		return contenuFichier;
 	}
 }
