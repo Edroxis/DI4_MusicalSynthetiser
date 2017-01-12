c0689f7e6c40fe25f15c297da43df401fdec5d94
Julien Amoros <ju.amoros@laposte.net>
2017-01-12 17:12:06 +0100

    Génération JavaDoc

--- Projet_DI4_SynthetiseurMusical/src/Controller/MainController.java
+++ Projet_DI4_SynthetiseurMusical/src/Controller/MainController.java
@@ -9,9 +9,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
 
 import javax.sound.sampled.LineUnavailableException;
 import javax.swing.JFileChooser;
--- Projet_DI4_SynthetiseurMusical/src/Main/Main.java
+++ Projet_DI4_SynthetiseurMusical/src/Main/Main.java
@@ -1,12 +1,13 @@
 package Main;
 
-import java.io.IOException;
-
 import javax.sound.sampled.LineUnavailableException;
 
 import Controller.*;
 import Model.*;
 
+/**
+ * Pour l'interface graphique, exécuter Controller.MainController
+ */
 public class Main {
 
 	public static void main(String[] args) throws LineUnavailableException {
